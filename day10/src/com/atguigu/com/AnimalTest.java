af05ae38beaafb990b1c89c5747c6df8dd71b1ad
gaoyong11 <gaoyongs72@163.com>
2020-10-12 10:50:00 +0800

    day10

--- day10/src/com/atguigu/com/AnimalTest.java
+++ day10/src/com/atguigu/com/AnimalTest.java
@@ -1,5 +1,15 @@
 package com.atguigu.com;
 
+/*
+ * java中4种权限修饰符及使用权限
+ * 	修饰符		类内部	同一个包	不同包的子类	同一个工程
+ * 	private		yes	
+ * 	缺省			yes		yes
+ * 	protected	yes		yes		yes
+ * 	public		yes		yes		yes			yes	
+ * 
+ * 	类的修饰只能使用 public、缺省来修饰
+ */
 public class AnimalTest {
 	public static void main(String[] args) {
 		Animal animal = new Animal();
